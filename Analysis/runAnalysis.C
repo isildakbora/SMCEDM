01339fa926a2cca71f6817adc4f62b1073f8b67d
isildakbora <bora.isildak@cern.ch>
2018-08-16 01:49:50 +0200

    run analyis on different bkg samples

--- Analysis/runAnalysis.C
+++ Analysis/runAnalysis.C
@@ -1,7 +1,12 @@
 
 void runAnalysis() {
-	gROOT->ProcessLine(".L SMCEDM_Analysis.C");
-	gROOT->ProcessLine("SMCEDM_Analysis m");
-	gROOT->ProcessLine("m.Loop()");
-	gROOT->ProcessLine("TBrowser b");
+	TString filename[1] =  {"WJets"};
+	
+	for(int i = 0; i <(int)(sizeof(filename)/sizeof(TString)); i++)
+	{
+		gROOT->ProcessLine(".L SMCEDM_Analysis.C");
+		gROOT->ProcessLine("SMCEDM_Analysis m(\"" + filename[i] + ".root\")");
+		gROOT->ProcessLine("m.Loop(\"" + filename[i]+ +"_out.root\")");
+		gROOT->ProcessLine("TBrowser b");
+	}
 }
