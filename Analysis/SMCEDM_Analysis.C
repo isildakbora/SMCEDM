190308b3246de44984deeee471d00797e926fb3c
Bora Isildak <bora.isildak@cern.ch>
2019-12-14 12:26:47 +0300

    Update SMCEDM_Analysis.C

--- Analysis/SMCEDM_Analysis.C
+++ Analysis/SMCEDM_Analysis.C
@@ -1,238 +1,230 @@
-#define SMCEDM_Analysis_cxx
-#include "SMCEDM_Analysis.h"
-#include <TH2.h>
-#include <TStyle.h>
-#include <TCanvas.h>
-#include <vector>
-#include <TLorentzVector.h>
-#include <TMatrixDSym.h>
-#include <TMatrixDSymEigen.h>
-#include <util.h>
+#include "leptons.h"
+#include "jets.h"
+#include "candidate.h"
 
-void SMCEDM_Analysis::Loop(TString outfilename) {
-    //   In a ROOT session, you can do:
-    //      root> .L SMCEDM_Analysis.C
-    //      root> SMCEDM_Analysis t
-    //      root> t.GetEntry(12); // Fill t data members with entry number 12
-    //      root> t.Show();       // Show values of entry 12
-    //      root> t.Show(16);     // Read and show values of entry 16
-    //      root> t.Loop();       // Loop on all entries
-    //
 
-    //     This is the loop skeleton where:
-    //    jentry is the global entry number in the chain
-    //    ientry is the entry number in the current Tree
-    //  Note that the argument to GetEntry must be:
-    //    jentry for TChain::GetEntry
-    //    ientry for TTree::GetEntry and TBranch::GetEntry
-    //
-    //       To read only selected branches, Insert statements like:
-    // METHOD1:
-    //    fChain->SetBranchStatus("*",0);  // disable all branches
-    //    fChain->SetBranchStatus("branchname",1);  // activate branchname
-    // METHOD2: replace line
-    //    fChain->GetEntry(jentry);       //read all branches
-    //by  b_branchname->GetEntry(ientry); //read only this branch
+void semi_leptonic()
+{
+	int nthreads = 4;
+	ROOT::EnableImplicitMT(nthreads);
 
-    TFile *outfile_ = new TFile(outfilename, "RECREATE");
+	//auto file = new TFile("tag_1_delphes_events.root");
+	auto file = new TFile("semi_lep.root");
+	TTreeReader myReader("Delphes", file);
 
-    //--- book the tree -----------------------
-    TTree *outTree_ = new TTree("outTree", "outTree");
+	auto outfile = new TFile("outfile_name.root", "RECREATE");
 
-    //outTree_->Branch("nvtx"                 ,&nVtx_              ,"nVtx_/I");
-    int dummy_index(1), idx_1(0), idx_2(0), nJets_, nLeptons_, nBJets_;
-    float ht_, met_, metPhi_, metSig_, sphericity_, foxWolfram_[4], aplanarity_, mW_, mTop_, ptTop_, yTop_;
-    float mW_ref = 80;
-    float jetPt_[5];
+	//Jet Definitions
+	TTreeReaderValue<int>            rv_Jet_size(myReader, "Jet_size");
+	TTreeReaderArray<float>          ra_Jet_pT(myReader,   "Jet.PT");
+	TTreeReaderArray<float>          ra_Jet_Eta(myReader,  "Jet.Eta");
+	TTreeReaderArray<float>          ra_Jet_Phi(myReader,  "Jet.Phi");
+	TTreeReaderArray<float>          ra_Jet_Mass(myReader, "Jet.Mass");
+	TTreeReaderArray<unsigned int>   ra_Jet_BTag(myReader, "Jet.BTag");
 
-    TLorentzVector aux_jetP4(0, 0, 0, 0);
-    TLorentzVector had_W_candidateP4(0, 0, 0, 0);
-    TLorentzVector had_top_candidateP4(0, 0, 0, 0);
+	//Electron Definitions
+	TTreeReaderValue<int>            rv_Electron_size(myReader,  "Electron_size");
+	TTreeReaderArray<float>          ra_Electron_pT(myReader,    "Electron.PT");
+	TTreeReaderArray<float>          ra_Electron_Eta(myReader,    "Electron.Eta");
+	TTreeReaderArray<float>          ra_Electron_Phi(myReader,    "Electron.Phi");
+	TTreeReaderArray<float>          ra_Electron_Energy(myReader,   "Electron.T");
+	TTreeReaderArray<int>            ra_Electron_Charge(myReader, "Electron.Charge");
 
-    std::vector<float> delta_mW_candidate;
-    std::vector<float> deltaR;
-    std::vector<std::pair <int, int>> light_jet_pair_index;
+	//Muon Definitions
+	TTreeReaderValue<int>            rv_Muon_size(myReader,  "Muon_size");
+	TTreeReaderArray<float>          ra_Muon_pT(myReader,    "Muon.PT");
+	TTreeReaderArray<float>          ra_Muon_Eta(myReader,    "Muon.Eta");
+	TTreeReaderArray<float>          ra_Muon_Phi(myReader,    "Muon.Phi");
+	TTreeReaderArray<float>          ra_Muon_Energy(myReader,   "Muon.T");
+	TTreeReaderArray<int>            ra_Muon_Charge(myReader, "Muon.Charge");
 
-    outTree_->Branch("nJets", &nJets_, "nJets_/I");
-    outTree_->Branch("nLeptons", &nLeptons_, "nLeptons_/I");
-    outTree_->Branch("nBJets", &nBJets_, "nBJets_/I");
-    outTree_->Branch("jetPt", &jetPt_, "jetPt_[5]/F");
-    outTree_->Branch("ht", &ht_, "ht_/F");
-    outTree_->Branch("met", &met_, "met_/F");
-    outTree_->Branch("metPhi", &metPhi_, "metPhi_/F");
-    /*outTree_->Branch("metSig"               ,&metSig_            ,"metSig_/F");*/
-    outTree_->Branch("sphericity", &sphericity_, "sphericity_/F");
-    outTree_->Branch("aplanarity", &aplanarity_, "aplanarity_/F");
-    outTree_->Branch("foxWolfram", &foxWolfram_, "foxWolfram_[4]/F");
-    outTree_->Branch("mW", &mW_, "mW_/F");
-    outTree_->Branch("mTop", &mTop_, "mTop_/F");
-    outTree_->Branch("ptTop", &ptTop_, "ptTop/F");
-    outTree_->Branch("yTop", &yTop_, "yTop_/F");
-    /*  outTree_->Branch("mTTbar"               ,&mTTbar_            ,"mTTbar_/F");
-        outTree_->Branch("yTTbar"               ,&yTTbar_            ,"yTTbar_/F");
-        outTree_->Branch("ptTTbar"              ,&ptTTbar_           ,"ptTTbar_/F");
-        outTree_->Branch("dRbbTop"              ,&dRbbTop_           ,"dRbbTop_/F");*/
-    //------------------------------------------------------------------
-    // Define histograms
 
-    //------------------------------------------------------------------
+	//MET Definitions
+	TTreeReaderArray<float>          ra_MissingET_MET(myReader,   "MissingET.MET");
 
-    if (fChain == 0) return;
+	// Histogram Definitions
+	auto histo_bjet_pT      = new TH1F("histo_bjet_pT", "histo_bjet_pT", 200, 0, 2000);
+	auto histo_light_jet_pT = new TH1F("histo_light_jet_pT", "histo_light_jet_pT", 200, 0, 2000);
+	auto histo_had_top_mass = new TH1F("histo_had_top_mass", "histo_had_top_mass", 1000, 0, 1000);
+	auto histo_had_top_eta  = new TH1F("histo_had_top_eta", "histo_had_top_eta", 100, -5, 5);
+	auto histo_had_top_phi  = new TH1F("histo_had_top_phi", "histo_had_top_phi", 30, -3.14, 3.14);
 
-    Long64_t nentries = fChain->GetEntriesFast();
 
-    Long64_t nbytes = 0, nb = 0;
-    for (Long64_t jentry = 0; jentry < nentries; jentry++) {
-        Long64_t ientry = LoadTree(jentry);
-        if (ientry < 0) break;
-        nb = fChain->GetEntry(jentry);
-        nbytes += nb;
+	auto n_events = myReader.GetEntries(1);
+	std::cout << "n_events:" << n_events << std::endl;
 
-        nJets_    = Jet_size;
-        nLeptons_ = Electron_size + Muon_size;
+	int n_btag;
+	int n_btag_cut = 0;	
+	int n_light_jet;
+	int n_light_jet_cut = 0;
+	int n_lepton_cut = 0;
+	int n_MET_cut  = 0;
+	int n_events_passed = 0;
+	float MET_threshold = 0.0;
+	int n_positive_electrons, n_negative_electrons, n_positive_muons, n_negative_muons;
 
-        nBJets_ = 0;
+	// Loop over Events
+	for (int i_event = 0; i_event < n_events; ++i_event)
+	{
+		n_btag = 0;
+		n_positive_electrons = 0;
+		n_negative_electrons = 0;
+		n_positive_muons = 0;
+		n_negative_muons = 0;
 
-        // sort jetpT in a descending order
-        std::sort(std::begin(Jet_PT), std::end(Jet_PT), greater<float>());
+		bool bool_lepton_cut(1), bool_b_jet_cut(1), bool_MET_cut(1), bool_light_jet_cut(1);
 
-        // make sure that jetPt_ array is zeroed
-        std::fill(std::begin(jetPt_), std::end(jetPt_), 0.);
+		myReader.SetEntry(i_event);
 
-        for (int i = 0; i < Jet_size; ++i) {
-            if (i < 5)
-                jetPt_[i] = Jet_PT[i];
+		// put all electrons to a container (vector of leptons object)
+		std::vector<leptons> v_electrons;
+		leptons dummy_electron;
+		//******* count the number of negative and positive electrons *******//
+		for (int i_electron = 0; i_electron < *rv_Electron_size; ++i_electron)
+		{
+			dummy_electron.setPt(ra_Electron_pT.At(i_electron));
+			dummy_electron.setEta(ra_Electron_Eta.At(i_electron));
+			dummy_electron.setPhi(ra_Electron_Phi.At(i_electron));
+			dummy_electron.setM(0.511/1000);
+			dummy_electron.setQ(ra_Electron_Charge.At(i_electron));
+			dummy_electron.setPDGID(11);
 
-            if (Jet_BTag[i] == 1)
-                nBJets_++;
-        }
+			v_electrons.push_back(dummy_electron);
+		}
+		
+		// put all muons to a container (vector of leptons object)
+		std::vector<leptons> v_muons;
+		leptons dummy_muon;
+		//******* count the number of negative and positive muons *******//
+		for (int i_muon = 0; i_muon < *rv_Muon_size; ++i_muon)
+		{
+			dummy_muon.setPt(ra_Muon_pT.At(i_muon));
+			dummy_muon.setEta(ra_Muon_Eta.At(i_muon));
+			dummy_muon.setPhi(ra_Muon_Phi.At(i_muon));
+			dummy_muon.setM(105/1000);
+			dummy_muon.setQ(ra_Muon_Charge.At(i_muon));
+			dummy_muon.setPDGID(13);
 
-        ht_     = ScalarHT_HT[0];
-        met_    = MissingET_MET[0];
-        metPhi_ = MissingET_Phi[0];
+			v_muons.push_back(dummy_muon);
+		}
 
-        /////////////Calculate Event Shapes/////////////
-        float sumE(0.0), sumP2(0.0), sumPxx(0.0), sumPxy(0.0), sumPxz(0.0), sumPyy(0.0), sumPyz(0.0), sumPzz(0.0);
+		// put all jets to a container (vector of jets object)
+		std::vector<jets> v_jets;
+		jets dummy_jet;
+		//******* count the number of negative and positive muons *******//
+		for (int i_jet = 0; i_jet < *rv_Jet_size; ++i_jet)
+		{
+			dummy_jet.setPt(ra_Jet_pT.At(i_jet));
+			dummy_jet.setEta(ra_Jet_Eta.At(i_jet));
+			dummy_jet.setPhi(ra_Jet_Phi.At(i_jet));
+			dummy_jet.setM(ra_Jet_Mass.At(i_jet));
+			dummy_jet.setBTag(ra_Jet_BTag.At(i_jet));
 
-        vector<TLorentzVector> jetP4;
-        vector<TLorentzVector> non_b_jetsP4;
-        vector<TLorentzVector> b_jetsP4;
+			v_jets.push_back(dummy_jet);
+		}
 
-        for (int i = 0; i < Jet_size; ++i) {
+		if(v_electrons.size() + v_muons.size() < 1)
+		{
+			bool_lepton_cut = 0; 
+			n_lepton_cut++;
+		}
+		 
+		std::vector<jets> v_light_jets;
 
-            aux_jetP4.SetPtEtaPhiM(Jet_PT[i], Jet_Eta[i], Jet_Phi[i], Jet_Mass[i]);
+		for (int i_jet = 0; i_jet < v_jets.size(); ++i_jet)
+		{
+			if(v_jets.at(i_jet).getBTag()!=1)
+			{
+				v_light_jets.push_back(v_jets.at(i_jet));
+			}
+		}
 
-            if (Jet_BTag[i] == 0)
-                non_b_jetsP4.push_back(aux_jetP4);
-            else
-                b_jetsP4.push_back(aux_jetP4);
+		std::vector<jets> v_b_jets;
 
-            jetP4.push_back(aux_jetP4);
+		for (int i_jet = 0; i_jet < v_jets.size(); ++i_jet)
+		{
+			if(v_jets.at(i_jet).getBTag()==1)
+			{
+				v_b_jets.push_back(v_jets.at(i_jet));
+			}
+		}
 
-            sumE   += aux_jetP4.Energy();
-            sumP2  += aux_jetP4.P()  * aux_jetP4.P();
-            sumPxx += aux_jetP4.Px() * aux_jetP4.Px();
-            sumPxy += aux_jetP4.Px() * aux_jetP4.Py();
-            sumPxz += aux_jetP4.Px() * aux_jetP4.Pz();
-            sumPyy += aux_jetP4.Py() * aux_jetP4.Py();
-            sumPyz += aux_jetP4.Py() * aux_jetP4.Pz();
-            sumPzz += aux_jetP4.Pz() * aux_jetP4.Pz();
-        }
+		n_btag = *rv_Jet_size - v_light_jets.size();
 
-        if (sumP2 > 0) {
-            float sumPij[4] = {0.0, 0.0, 0.0, 0.0};
-            for (int k1 = 0; k1 < jetP4.size(); k1++) {
-                for (int k2 = k1 + 1; k2 < jetP4.size(); k2++) {
-                    float cosDPhi = cos(jetP4[k1].DeltaPhi(jetP4[k2]));
-                    float factor  = jetP4[k1].P() * jetP4[k2].P() / (sumE * sumE);
-                    for (int i = 0; i < 4; i++) {
-                        sumPij[i] += factor * ROOT::Math::legendre(i, cosDPhi); //legendre 0 = 1
-                    }
-                }
-            }
-            for (int i = 0; i < 4; i++) {
-                foxWolfram_[i] = sumPij[i];
-            }
-            //---- compute sphericity -----------------------
-            float Txx = sumPxx / sumP2;
-            float Tyy = sumPyy / sumP2;
-            float Tzz = sumPzz / sumP2;
-            float Txy = sumPxy / sumP2;
-            float Txz = sumPxz / sumP2;
-            float Tyz = sumPyz / sumP2;
-            TMatrixDSym T(3);
-            T(0, 0) = Txx;
-            T(0, 1) = Txy;
-            T(0, 2) = Txz;
-            T(1, 0) = Txy;
-            T(1, 1) = Tyy;
-            T(1, 2) = Tyz;
-            T(2, 0) = Txz;
-            T(2, 1) = Tyz;
-            T(2, 2) = Tzz;
-            TMatrixDSymEigen TEigen(T);
-            TVectorD eigenValues(TEigen.GetEigenValues());
-            sphericity_ = 1.5 * (eigenValues(1) + eigenValues(2));
-            aplanarity_ = 1.5 * eigenValues(2);
-        }
-        //////////////////////////
+		if(v_light_jets.size() < 2)
+		{
+			bool_light_jet_cut = 0;
+			n_light_jet_cut++;
+		}
 
-        //////Calculate W (from light quarks)//////////
-        mW_ = 0.;
+		if(n_btag < 2)
+		{
+			bool_b_jet_cut = 0;
+			n_btag_cut++;
+		}
+		
+		//******* skip the event if MET < threshold *******//
+		//std::cout << "MET:" << ra_MissingET_MET[0] << std::endl;
+		if (ra_MissingET_MET[0] < MET_threshold)
+		{
+			n_MET_cut++;
+			bool_MET_cut = 0;
+			continue;
+		}
 
-        if (non_b_jetsP4.size() > 1) {
-            for (int i = 0; i < non_b_jetsP4.size(); ++i) {
-                for (int j = i + 1; j < non_b_jetsP4.size(); ++j) {
-                    // meanwhile store the indices of jet pairs
-                    light_jet_pair_index.push_back(std::make_pair (i, j));
+	
+		if(bool_lepton_cut==0 || bool_MET_cut==0 || bool_light_jet_cut==0 || bool_b_jet_cut==0)
+		{
+			continue;
+		}
 
-                    /// find the invariant mass difference between all possible non-b dijets and the mass of W (mW_ref)
-                    delta_mW_candidate.push_back(abs(((non_b_jetsP4[i] + non_b_jetsP4[j]).M()) - mW_ref));
-                }
-            }
+		std::vector<candidate> v_W_candidates;
+		
+		for (int i_jet = 0; i_jet < v_light_jets.size(); ++i_jet)
+		{
+			for (int j_jet = 0; j_jet < v_light_jets.size(); ++j_jet)
+			{
+				if(i_jet < j_jet)
+				{
+					candidate dummy_candidate(v_light_jets.at(i_jet), v_light_jets.at(j_jet));
+					v_W_candidates.push_back(dummy_candidate);
+				}
+			}
+		}
 
-            /*  for (int i = 0; i < delta_mW_candidate.size(); ++i){std::cout << mW_ref + delta_mW_candidate[i] << ",";}std::cout << std::endl;
-                for (int i = 0; i < light_jet_pair_index.size(); ++i){std::cout << "(" <<light_jet_pair_index[i].first << "," <<light_jet_pair_index[i].second << ")"<< ",";}std::cout << std::endl;*/
+		std::vector<candidate> v_had_top_candidates;
 
-            //
-            auto min_delta_mW = std::min_element(delta_mW_candidate.begin(), delta_mW_candidate.end());
-            auto light_jet_pair_idx = std::distance(std::begin(delta_mW_candidate), min_delta_mW);
-            idx_1 = light_jet_pair_index[light_jet_pair_idx].first;
-            idx_2 = light_jet_pair_index[light_jet_pair_idx].second;
+		for (int i_w_candidate = 0; i_w_candidate < v_W_candidates.size(); ++i_w_candidate)
+		{
+			for (int j_bjet = 0; j_bjet < v_b_jets.size(); ++j_bjet)
+			{
+				candidate dummy_candidate(v_W_candidates.at(i_w_candidate), v_b_jets.at(j_bjet));
+				v_had_top_candidates.push_back(dummy_candidate);
+			}
+		}
 
-            // construct the hadronic W four-momenta from two light quarks
-            had_W_candidateP4 = non_b_jetsP4[idx_1] + non_b_jetsP4[idx_2];
-            mW_ = had_W_candidateP4.M();
+		std::sort(v_had_top_candidates.begin(), v_had_top_candidates.end(),[](candidate first, candidate second) {return abs(first.getM()- 173.) < abs(second.getM()- 173.);});
 
-            // Construct hadronic top quark
-            // find the W-bjet system that gives the minimum deltaR
-            for (int i = 0; i < b_jetsP4.size(); ++i) {
-                deltaR.push_back(had_W_candidateP4.DeltaR(b_jetsP4[i]));
-            }
-            auto min_deltaR = std::min_element(deltaR.begin(), deltaR.end());
-            auto idx_min_deltaR = std::distance(std::begin(deltaR), min_deltaR);
-            if (nBJets_ > 0) {
-                had_top_candidateP4 =  had_W_candidateP4 + b_jetsP4[idx_min_deltaR];
-                mTop_  = had_top_candidateP4.M();
-                ptTop_ = had_top_candidateP4.Pt();
-                yTop_  = had_top_candidateP4.Rapidity();
-            }
 
-            ///Clean the objects
-            delta_mW_candidate.clear();
-            light_jet_pair_index.clear();
-            deltaR.clear();
-        }
+		histo_had_top_mass->Fill(v_had_top_candidates.at(0).getM());
+		histo_had_top_eta->Fill(v_had_top_candidates.at(0).get4P().Eta());
+		histo_had_top_phi->Fill(v_had_top_candidates.at(0).get4P().Phi());
 
-        /// Define basic selection criteria
-        bool mW_cut = abs(mW_ - mW_ref) < 15.;
-        bool n_light_quark = (nJets_ - nBJets_) > 1;
+		n_events_passed++;
+	}
 
-        /// Fill the tree after basic selections
-        if (mW_cut * n_light_quark)
-            outTree_->Fill();
-    }
+	std::cout << "n_light_jet_cut: " << n_light_jet_cut << std::endl;
+	std::cout << "n_btag_cut: " << n_btag_cut << std::endl;
+	std::cout << "n_lepton_cut: " << n_lepton_cut << std::endl;
+	std::cout << "n_MET_cut: " << n_MET_cut << std::endl;
+	std::cout << "n_events_passed/n_events: " << n_events_passed << "/" << n_events << std::endl;
 
-    outfile_->Write();
-    outfile_->Close();
+	TCanvas *canvas1 = new TCanvas("canvas1", "canvas1", 600, 600); 
+	histo_had_top_mass->Draw("HIST");
+
+	TCanvas *canvas2 = new TCanvas("canvas2", "canvas2", 600, 600);
+	histo_had_top_eta->Draw("HIST");
+
+	TCanvas *canvas3 = new TCanvas("canvas3", "canvas3", 600, 600);
+	histo_had_top_phi->Draw("HIST");
 }
